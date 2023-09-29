c4c2ebca45e1430f70e17bd0b1a5126d978859ce
Pedro Coelho <115360958+PedroCoelho128@users.noreply.github.com>
2023-09-28 22:19:09 -0300

    Update index.tsx

--- Atividade 4/src/pages/tarefas/index.tsx
+++ Atividade 4/src/pages/tarefas/index.tsx
@@ -1,32 +1,31 @@
-import { Link } from "react-router-dom";
+iimport { Link } from "react-router-dom";
 
 export default function Tarefas() {
     return(
         <>
-            <h1>Tarefas sobre de Casa</h1>
+            <h1>Tarefas a se fazer em Casa</h1>
             <p>
-            <h2>Definição de Objetivos:</h2>
-            <br/>
-            Estabelecer Parcerias: Identificar e estabelecer parcerias com escolas locais, 
-            organizações da sociedade civil e agências governamentais para colaborar na implementação do projeto.
-            <br/>
-            Recrutamento de Educadores: Recrutar educadores qualificados e comprometidos com a missão do projeto.
-            <br/>
-            Desenvolvimento Curricular: Desenvolver um currículo de alta qualidade que atenda às necessidades de desenvolvimento das crianças e promova uma educação inclusiva.
-            <br/>
-            Infraestrutura: Melhorar a infraestrutura das escolas e locais de aprendizado, garantindo um ambiente seguro e adequado.
-            <br/>
-            Capacitação de Professores: Realizar workshops e programas de formação para educadores, focando em pedagogias modernas e práticas de ensino eficazes.
-            <br/>
-            Atividades de Engajamento dos Pais: Organizar oficinas e reuniões para os pais, incentivando-os a se envolverem na educação de seus filhos.
-            <br/>
-            Avaliação e Monitoramento: Estabelecer indicadores de sucesso e implementar sistemas de avaliação contínua para medir o progresso do projeto.
-            <br/>
-            Divulgação e Sensibilização: Promover o projeto na comunidade e sensibilizar os pais e cuidadores sobre a importância da educação infantil.
-            <br/>
-            Expansão Gradual: Planejar e implementar a expansão gradual do projeto para atender a um número crescente de crianças ao longo do tempo.
-            <br/>
-            Avaliação de Impacto: Conduzir avaliações de impacto para medir o sucesso do projeto e fazer ajustes conforme necessário para alcançar os objetivos estabelecidos.
+                <h2>Lista de Tarefas:</h2>
+                <br/>
+                1. Limpar e organizar a casa.
+                <br/>
+                2. Preparar refeições saudáveis para a família.
+                <br/>
+                3. Estabelecer um cronograma de estudo ou trabalho em casa.
+                <br/>
+                4. Fazer exercícios físicos ou praticar yoga para manter-se ativo.
+                <br/>
+                5. Realizar atividades de aprendizado online ou cursos para desenvolvimento pessoal.
+                <br/>
+                6. Realizar tarefas de jardinagem ou cuidar das plantas em casa.
+                <br/>
+                7. Ler livros ou eBooks para expandir o conhecimento.
+                <br/>
+                8. Realizar videochamadas ou reuniões virtuais com amigos e familiares.
+                <br/>
+                9. Praticar hobbies como pintura, costura, ou tocar um instrumento musical.
+                <br/>
+                10. Manter um diário ou fazer meditação para cuidar da saúde mental.
             </p>
             <hr/>
             <Link to='/'>Página inicial</Link><br/>
@@ -34,4 +33,6 @@ export default function Tarefas() {
             <Link to='/sobre'>Sobre</Link>
         </>
     )
+}
+
 }
